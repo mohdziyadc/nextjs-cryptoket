f8b6fa950153668eff0c8fb93b9bcc237e732679
Mohd Ziyad <mohdziyad@Mohds-MacBook-Air.local>
2023-01-17 22:38:48 +0530

    Final changes

--- pages/graphExample.js
+++ pages/graphExample.js
@@ -2,23 +2,23 @@ import { useQuery, gql } from "@apollo/client";
 import { useMoralis } from "react-moralis";
 
 const GET_ACTIVE_ITEMS = gql`
-  query GetOwnedNfts($owner: Bytes!) {
-    nfts(where: { to: $owner }) {
-      id
-      from
-      to
-      tokenUri
-      price
+    query GetOwnedNfts($owner: Bytes!) {
+        nfts(where: { to: $owner }) {
+            id
+            from
+            to
+            tokenUri
+            price
+        }
     }
-  }
 `;
 
 export default function GraphExample() {
-  const { account } = useMoralis();
-  console.log(account);
-  const { loading, error, data } = useQuery(GET_ACTIVE_ITEMS, {
-    variables: { owner: account },
-  });
-  console.log(data);
-  return <div>hi</div>;
+    const { account } = useMoralis();
+    console.log(account);
+    const { loading, error, data } = useQuery(GET_ACTIVE_ITEMS, {
+        variables: { owner: account },
+    });
+    console.log(data);
+    return <div>hi</div>;
 }
